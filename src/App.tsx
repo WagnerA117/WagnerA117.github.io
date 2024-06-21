5ba6eb5f49df36b876d3a92f8ceb298f31d5d102
Ahren <ahren@meetingpal.co.za>
2024-06-21 17:28:01 +0200

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<div>
+			<div className="bg-red-500 h-screen w-full ">
 				<p> I made a change </p>
 			</div>
+			<div className="h-full bg-white">more content</div>
 		</>
 	);
 }
