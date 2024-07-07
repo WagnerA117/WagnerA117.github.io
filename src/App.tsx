6f56c95018f6400c3989bfe970c39a8846e303eb
Ahren <ahren@meetingpal.co.za>
2024-07-07 22:42:45 +0200

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<div className="w-screen h-screen bg-brand-base bg-yellow ">
+			<div className="w-screen h-screen bg-blue-900">
 				<p> I made a change </p>
 			</div>
 		</>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-import colors from "./src/theme/colors";
 
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
-			colors: colors,
+			colors: {},
 		},
 	},
 	plugins: [],
