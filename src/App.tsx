2627750023f4ffa0c5b99b642870976e08eda540
Ahren <ahren@meetingpal.co.za>
2024-07-09 00:32:21 +0200

    initial download functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
 
+import resume from "./resume/resume.pdf";
+
 function App() {
 	return (
 		<>
-			<div className="w-screen h-screen bg-blue-900">
+			<div className="w-screen h-screen bg-blue-100">
 				<p> I made a change </p>
+				<a href={resume} download>
+					<button className="px-4 rounded-md hover:bg-white ">
+						Download Resume
+					</button>
+				</a>
 			</div>
 		</>
 	);
