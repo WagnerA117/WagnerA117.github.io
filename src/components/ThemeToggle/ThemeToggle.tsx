47c54b3ecfd7272adc10325c2955b665063bcf24
Ahren <ahren@meetingpal.co.za>
2024-07-24 22:33:00 +0200

    added some changes

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,21 +1,21 @@
 import {Moon, Sun} from "lucide-react";
 
-import {Button} from "@/components/ui/button";
+import {Button} from "../ui/button/button";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
 	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {useTheme} from "@/components/theme-provider";
+} from "../ui/dropdown/dropdown";
+import {useTheme} from "../../theme/ThemeProvider";
 
-export function ModeToggle() {
+export function ThemeToggle() {
 	const {setTheme} = useTheme();
 
 	return (
 		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant="outline" size="icon">
+			<DropdownMenuTrigger asChild className="bg-black">
+				<Button variant="destructive" size="icon">
 					<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 					<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 					<span className="sr-only">Toggle theme</span>
