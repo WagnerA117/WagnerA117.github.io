55f298b0d337157178677179e1b27011ac28f048
Ahren <ahren@meetingpal.co.za>
2024-08-25 20:46:04 +0200

    pre radix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import { Card } from "ui/card";
+
 export const Home = () => {
 	return (
 		<>
 			<ModeToggle />
-			<div>this will be the </div>
+			<Card></Card>
+
+			<div className=" dark:text-slate-100 text-slate-900">This is a test</div>
 		</>
 	);
 };
