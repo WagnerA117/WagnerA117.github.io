d1bc9af13c4cdd446ec44dbdad07062e951f110e
Ahren <ahren@meetingpal.co.za>
2024-11-26 21:07:33 +0200

    some adjustments

--- src/components/typography/typography.tsx
+++ src/components/typography/typography.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export function H1({ children }: PropsWithChildren) {
 	return (
-		<h1 className="text-4xl font-extrabold tracking-tight text-center align-bottom  scroll-m-20 lg:text-5xl">
+		<h1 className="text-4xl font-extrabold tracking-tight text-center align-bottom scroll-m-20 lg:text-5xl">
 			{children}
 		</h1>
 	);
